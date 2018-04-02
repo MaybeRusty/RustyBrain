c7cc35cf621a926f3f0825d7b7a10985210eff1f
MaybeRusty <1421819341@qq.com>
2018-04-02 03:44:38 -1000

    添加表单校验

--- admin/src/views/nodetree/components/OperaModal.vue
+++ admin/src/views/nodetree/components/OperaModal.vue
@@ -8,10 +8,10 @@
         </p>
         <div style="text-align:center">
             <Form ref="formData" :model="formData" :rules="ruleValidate" :label-width="80">
-                <FormItem label="Name" prop="Name">
+                <FormItem label="Name" prop="Name" :error="nameState">
                     <Input v-model="formData.Name" @on-change="formBind" placeholder="Please input this node name..."></Input>
                 </FormItem>
-                <FormItem label="IdentifyId" prop="IdentifyId">
+                <FormItem label="IdentifyId" prop="IdentifyId" :validateStatus="formStatus.identifyStatus">
                     <Input v-model="formData.IdentifyId" @on-change="formBind" placeholder="Please input this node identify number..."></Input>
                 </FormItem>
                 <FormItem label="Student" prop="is_Student">
@@ -21,10 +21,10 @@
                     </i-switch>
                 </FormItem>
                 <div v-if="formData.is_Student">
-        	        <FormItem label="Patriarch Name" prop="pName">
+        	        <FormItem label="Patriarch Name" prop="pName" :validateStatus="formStatus.pNameStatus">
         	            <Input v-model="formData.pName" @on-change="formBind" placeholder="Please input patriarch name..."></Input>
         	        </FormItem>
-        	        <FormItem label="Patriarch Contact" prop="pContact">
+        	        <FormItem label="Patriarch Contact" prop="pContact" :validateStatus="formStatus.pContactStatus">
         	            <Input v-model="formData.pContact" @on-change="formBind"  placeholder="Please input patriarch contact..."></Input>
         	        </FormItem>
         	       </div>
@@ -38,6 +38,7 @@
     </Modal>
 </template>
 <script>
+	
     export default {
         name: 'OperaModal',
         props:
@@ -47,6 +48,40 @@
                 'currOpera'
         ],
         data () {
+        	const getStudent	= ()=>{
+        		return this.formData.is_Student;
+        	}
+        	const checkPname 	= (rule, value, callback) => {
+        		var student_status = getStudent()
+        		if(student_status){
+        			if(value === ''){
+        				callback(new Error('Can not be empty'));
+	        		}else if(value.length > 128){
+	        			callback(new Error('More than of max length'));
+	        		}else{
+	        			callback();
+	        		}
+        		}else{
+        			callback();
+        		}
+        		
+        	};
+        	const checkPcontact	= (rule, value, callback) => {
+        		var student_status = getStudent()
+				if(student_status){
+					if(value === ''){
+        				callback(new Error('Can not be empty'));
+	        		}else if(value.length != 11){
+	        			callback(new Error('Invalid phone number format'));
+	        		}else if(/\d{11}$/.test(value) === false){
+	        			callback(new Error('Invalid phone number format'));
+	        		}else{
+	        			callback();
+	        		}
+				}else{
+					callback();
+				}
+        	};
             return {
                 checkret: true,
                 saveDisabled: true,
@@ -61,39 +96,40 @@
                         { max: 60, message: 'More than of max length', trigger: 'change' }
                     ],
                     pName: [
-                        { required: true , message: 'Can not be empty', trigger: 'change'},
-                        { max: 128, message: 'More than of max length', trigger: 'change'}
+                    	{ validator: checkPname, trigger: 'blur'}
+                        
                     ],
                     pContact: [
-                    	{ required: true , message: 'Must be phone number', trigger: 'change', pattern: /\d{11}$/},
-                        { len: 11, message: 'invalid phone number', trigger: 'change'}
+                    	{ validator: checkPcontact, trigger: 'blur'}
                     ]
+                },
+                nameState: '',
+                formStatus: {
+                	nameStatus: false,
+                	identifyStatus: false,
+                	pNameStatus: false,
+                	pContactStatus: false
                 }
             }
         },
         watch:{
             formData:{
                 handler (val) {
-                    this.$Message.info(Object.values(val)[0])
-                   //  for (let i = 0; i < Object.values(val).length; i++) {
-                   //      Object.values(val)[i].validate((valid) => {
-                   //          if (valid) {
-                   //              this.saveDisabled = false
-                   //          } else {
-                   //              this.saveDisabled = true
-                   //          }
-                   //      })
-                   // }
-                    // this.$Message.info(this.saveDisabled ? "true" : "false")
-                    this.$refs['formData'].validate((valid) => {
-                        if (valid) {
-                            this.saveDisabled = false
-                        } else {
-                            this.saveDisabled = true
-                        }
-                    })
+//              	this.$Message.info(this.$refs['ruleForm2'].validate() ? "this is true" : "this is false")
+                	this.$refs.formData.validate((valid) => {
+                		if(valid){
+                			this.saveDisabled = false
+                		}else{
+                			this.saveDisabled = true
+                		}
+                	})
                 },
                 deep: true
+            },
+            'formData.is_Student': function(val){
+            	if(val){
+            		this.saveDisabled = true
+            	}
             }
         },
         methods:{
